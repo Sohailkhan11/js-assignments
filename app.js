5af60c477a41c9ae0b586841a8f9a15e8607a6a6
Sohail Ahmed <sohailalijamali3@gmail.com>
2021-06-25 04:04:25 -0700

    js chapter done

---
+++ app.js
@@ -0,0 +1,10 @@
+alert("Error !! Please Enter a valid Password")
+
+alert("Welcome To Js land  " + "\n" + "Happy Coding")
+
+alert("Welcome To Js Land")
+
+alert("Happy Coding :.")
+
+alert("Happy Coding !")
+checkbox=alert(" done with first chapter")
